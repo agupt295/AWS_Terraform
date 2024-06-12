2edf537df6b53ed1e87cc86b22b802aba6b52ea8
EC2 Default User <ec2-user@ip-172-31-41-170.ec2.internal>
2024-06-12 09:14:28 +0000

    comments in variables.tf file

--- variables.tf
+++ variables.tf
@@ -1,15 +1,18 @@
 # This is where to create variables
 
+# define number of Availability Zones
 variable "az_num" {
   type    = number
   default = 2
 }
 
+# define namespace
 variable "namespace" {
   type    = string
   default = "terraform-workshop"
 }
 
+# define VPC CIDR block to be used for deployment
 variable "vpc_cidr_block" {
   type    = string
   default = "10.0.0.0/16"
