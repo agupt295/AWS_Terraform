3e09775f711703cb39593bb9687c7a27585534fe
EC2 Default User <ec2-user@ip-172-31-41-170.ec2.internal>
2024-06-12 09:19:55 +0000

    IAM in main.tf

--- main.tf
+++ main.tf
@@ -84,3 +84,34 @@ data "aws_iam_policy_document" "assume_role" {
     actions = ["sts:AssumeRole"]
   }
 }
+
+# Create resources for IAM- allow our EC2 instances to read from an S3 bucket, as well get full access permissions to the RDS resources
+resource "aws_iam_role" "app" {
+  name               = "app"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  managed_policy_arns = [
+    data.aws_iam_policy.ssm_managed.arn,
+    data.aws_iam_policy.database.arn
+  ]
+}
+
+resource "aws_iam_role" "web_hosting" {
+  name               = "web_hosting"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  managed_policy_arns = [
+    data.aws_iam_policy.ssm_managed.arn,
+    data.aws_iam_policy.s3_ReadOnly.arn
+  ]
+}
+
+resource "aws_iam_instance_profile" "app" {
+  name = "app-profile"
+  role = aws_iam_role.app.name
+}
+
+resource "aws_iam_instance_profile" "web_hosting" {
+  name = "web-hosting-profile"
+  role = aws_iam_role.web_hosting.name
+}
