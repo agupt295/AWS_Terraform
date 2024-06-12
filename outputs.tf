f5f8912f43958e7bb453d6b7decf960273dce99b
Aryan Gupta <118144312+agupt295@users.noreply.github.com>
2024-06-12 20:19:52 +0530

    Update outputs.tf-export app for URL

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,14 @@
 # This is where to create outputs
+# creating three output variables to export the application load balancer URI, application load balancer URL, and CloudFront endpoint URL.
 
+output "alb_endpoint_uri" {
+  value = aws_lb.mission_app.dns_name
+}
+
+output "alb_endpoint_url" {
+  value = "https://${aws_lb.mission_app.dns_name}"
+}
+
+output "cloudfront_endpoint_url" {
+  value = "https://${aws_cloudfront_distribution.mission_app.domain_name}"
+}
